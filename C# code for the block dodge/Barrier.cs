d463e2f7aa1f12a006d0e3f2f5a13d1b91a6e6e2
Xingshuo Yan <68331868+332575672@users.noreply.github.com>
2020-11-30 21:07:30 +0800

    C# code for the block dodge

---
+++ C# code for the block dodge/Barrier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name +"Touch me");
+        Time.timeScale = 0;
+    }
+}
